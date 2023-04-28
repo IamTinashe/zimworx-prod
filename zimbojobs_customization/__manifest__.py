ce3f69c0c58396c0cdc30e6f5be9905d7c439999
Tinashe Zvihwati <tinashetp@gmail.com>
2023-04-28 19:16:16 +0200

    initialize_app

---
+++ zimbojobs_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Zimbojobs automation',
+    'version': '1.0.0',
+    'summary': 'Zimbojobs Customization',
+    'description': 'Zimbojobs Customization',
+    'summary': 'Zimbojobs Customization',
+    'category': 'Customization',
+    'author': 'Tinashe Zvihwati',
+    'website': 'https://zimworx.com',
+    'depends': ['hr'],
+    'images': ['static/zimbojobs.jfif'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
