1d23edf2a98e4afc600f14a8a8816b6cc5ea2801
Ashford Makwarimba <anesumukanya@gmail.com>
2023-06-13 11:30:29 +0200

    Update account_move.py

--- customer_vendor_statement/models/account_move.py
+++ customer_vendor_statement/models/account_move.py
@@ -3,4 +3,4 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move.line'
 
-    account_internal_type = fields.Selection(related='account_id.user_type_id.type', string="Internal Type", readonly=True, store=True)
+    # account_internal_type = fields.Selection(related='account_id.user_type_id.type', string="Internal Type", readonly=True, store=True)
