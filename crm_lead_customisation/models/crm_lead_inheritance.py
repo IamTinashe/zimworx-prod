a4e5c8deab002ae4ebc50f10c0f8d8c8aa5e7ba1
E.Makaudze <estainmakaudze@gmail.com>
2024-08-17 00:08:16 +0200

    FIX 2 DEFAULT VALUE FOR SALESPERSON FIELD IN REPLACEMENTS

--- crm_lead_customisation/models/crm_lead_inheritance.py
+++ crm_lead_customisation/models/crm_lead_inheritance.py
@@ -7,10 +7,10 @@ class CrmLeadInheritance(models.Model):
     _inherit = 'crm.lead'
 
 
-    @api.onchange('x_studio_seat_type')
-    def onchange_x_studio_seat_type(self):
-        """WHEN SEAT TYPE CHANGE TO REPLACEMENT UPDATE x_studio_sales_person WITH user_id"""
-        for record in self:
-            if record.x_studio_seat_type=='Replacement Seats' and record.x_studio_salesperson==False:
-                record.x_studio_salesperson = record.user_id
-
+    # @api.onchange('x_studio_seat_type')
+    # def onchange_x_studio_seat_type(self):
+    #     """WHEN SEAT TYPE CHANGE TO REPLACEMENT UPDATE x_studio_sales_person WITH user_id"""
+    #     for record in self:
+    #         if record.x_studio_seat_type=='Replacement Seats' and record.x_studio_salesperson==False:
+    #             record.x_studio_salesperson = record.user_id
+    #
