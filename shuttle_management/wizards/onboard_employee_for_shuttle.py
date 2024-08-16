b9e556f7de0cabb813ecabb9e2c5c09ebadac3e3
E.Makaudze <estainmakaudze@gmail.com>
2024-08-16 23:48:47 +0200

    FOR CHECKING OUT PURPOSES

--- shuttle_management/wizards/onboard_employee_for_shuttle.py
+++ shuttle_management/wizards/onboard_employee_for_shuttle.py
@@ -9,7 +9,9 @@ class OnboardEmployeeForShuttle(models.TransientModel):
     _description = 'Employee Onboard'
 
     employee_location = fields.Char()
+    quick_note = fields.Char()
     route_not_found = fields.Boolean(default=False)
+    schedule_found = fields.Boolean(default=False)
     shuttle_id = fields.Many2one('shuttles.model', string='Possible Shuttle', required=True)
     shuttle_schedule_ids=fields.One2many('onboard_shuttle_schedule.wizard', 'shuttle_id', string='shuttle_schedule Shuttle')
 
@@ -22,18 +24,72 @@ class OnboardEmployeeForShuttle(models.TransientModel):
         # if hr_employee_id.street ==False:
         #     raise ValidationError(_("Please Ensure the Street of the employee is filled"))
         #load the current employee location
-        res['employee_location']= f"{hr_employee_id.street} - {hr_employee_id.city}"
+        res['employee_location']= f"{hr_employee_id.street2} - {hr_employee_id.city}"
+
+
         #search for this location in route_end_location
-        route_end_location = self.env['route_end_location.model'].search([('route_end', '=', hr_employee_id.street)])
+        print(hr_employee_id.street,"emp")
+        route_end_location = self.env['route_end_location.model'].search([('route_end', '=', hr_employee_id.street2)])
         print(len(route_end_location))
         if len(route_end_location)==0:
             res['route_not_found']=True
             #No Shuttle found with that location return to give a validation error
+        else:
+            #the route exits and lest try to find if there is a shuttle for that route
+            for rec in route_end_location:
+                #search into shuttle for the route
+                shuttle_routes_id = self.env['shuttle_routes.model'].search([('main_route_end', '=', rec.id)])
+                shuttles_id = self.env['shuttles.model'].search([('shuttle_route', '=', shuttle_routes_id.ids)])
+                employee_schedules = self.env['employee_schedules.model'].search([('employee_id', '=', hr_employee_id.id)])
+                shuttle_lines=[]
+                #check if there is any shuttle with that route
+                if len(shuttles_id)!=0:
+                    for shuttle in shuttles_id:
+                        for rec in shuttle.shuttle_schedule_ids:
+                            #loop through the employee schdule just try to find a match of employee schudule and available schedule
+                            for record in employee_schedules:
+                                if set(record.weekday_id.ids).intersection(set(rec.weekday_id.ids)):
+                                    print("rec.departure_time",rec.departure_time)
+                                    print("record.departure_time",record.departure_time)
+                                    #find the one in the match if there is any which is free which match with schdule
+                                    if rec.departure_time == record.departure_time and rec.is_fully_booked !='fully_booked':
+                                        #if a match of schedule time pass that shuttle to shuttle id and break the whole loop
+                                        res['shuttle_id']=shuttle.id
+                                        res['schedule_found']=True
+                                        res['quick_note']=f'{shuttle.name} contains a possible slot on {record.departure_time} departure_time slots'
+                                        # Loop through the shuttle schedule so that it can be seen for display
+                                        for final_shuttle_schedule in shuttle.shuttle_schedule_ids:
+                                            shuttle_line={
+                                                'departure_time':final_shuttle_schedule.departure_time,
+                                                'booked_seating_capacity':final_shuttle_schedule.booked_seating_capacity,
+                                                'max_seating_capacity':final_shuttle_schedule.max_seating_capacity,
+                                                'is_fully_booked':final_shuttle_schedule.is_fully_booked,
+                                                'weekday_id':final_shuttle_schedule.weekday_id,
+                                            }
+                                            shuttle_lines.append((0, 0, shuttle_line))
+                                        res['shuttle_schedule_ids'] = shuttle_lines
+                                        break
+                                else:
+                                    res['schedule_found'] == False
+                                    departure_time=record.departure_time
+                                    possible_shuttle=shuttle
 
-
-
-
-
+                    #lest take instance where find a shuttle with a possible route without any of that schedule
+                    if res['schedule_found'] == False:
+                        res['shuttle_id'] = possible_shuttle.id
+                        res['schedule_found'] = False
+                        res['quick_note'] = f'{possible_shuttle.name} pass through this route but doesnt contains {departure_time} departure_time slot. Please consider add this Employee Schedule on this shuttle or find another schedule'
+                        # Loop through the shuttle schedule so that it can be seen for displa
+                        for final_shuttle_schedule in possible_shuttle.shuttle_schedule_ids:
+                            shuttle_line = {
+                                'departure_time': final_shuttle_schedule.departure_time,
+                                'booked_seating_capacity': final_shuttle_schedule.booked_seating_capacity,
+                                'max_seating_capacity': final_shuttle_schedule.max_seating_capacity,
+                                'is_fully_booked': final_shuttle_schedule.is_fully_booked,
+                                'weekday_id': final_shuttle_schedule.weekday_id,
+                            }
+                            shuttle_lines.append((0, 0, shuttle_line))
+                        res['shuttle_schedule_ids'] = shuttle_lines
         return res
 
 
