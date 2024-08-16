c87f243ad57a908e4fbe5ca211ee2491146532bc
E.Makaudze <estainmakaudze@gmail.com>
2024-08-17 00:03:28 +0200

    FIX DEFAULT VALUE FOR SALESPERSON FIELD IN REPLACEMENTS

---
+++ crm_lead_customisation/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Crm Lead Customisation',
+    'version': '24.0.1',
+    'summary': 'Invoices & Payments',
+    'author': 'Estain Makaudze',
+    'sequence': -100,
+    'description': """This is a module to customise CRM LEAD""",
+    'category': 'Accounting/Accounting',
+    'website': 'https://zimworx.odoo.com/',
+    'depends': ['contacts','mail'],
+    'data': [
+        # security -- data -- views --reports
+        # 'wizard/default_views.xml',
+        # 'views/default_views.xml',
+        # 'report/default_report.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'license': 'LGPL-3',
+}
---
+++ crm_lead_customisation/models/crm_lead_inheritance.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class CrmLeadInheritance(models.Model):
+    _inherit = 'crm.lead'
+
+
+    @api.onchange('x_studio_seat_type')
+    def onchange_x_studio_seat_type(self):
+        """WHEN SEAT TYPE CHANGE TO REPLACEMENT UPDATE x_studio_sales_person WITH user_id"""
+        for record in self:
+            if record.x_studio_seat_type=='Replacement Seats' and record.x_studio_salesperson==False:
+                record.x_studio_salesperson = record.user_id
+
