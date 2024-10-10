0522d5c05d04cf6dfab993297b9dc83c4535e8ea
E.Makaudze <estainmakaudze@gmail.com>
2024-10-10 04:35:51 +0200

    CONFIRM BUTTON NOW WORKING

--- shuttle_management/controllers/my_shuttle_controller.py
+++ shuttle_management/controllers/my_shuttle_controller.py
@@ -15,14 +15,14 @@ class ShuttleManagement(http.Controller):
             #render a fake login for asking for driver_id
             return request.redirect('/my_shuttle/login')
         else:
+            today = datetime.now()
             #get driver details and shuttle details
             shuttle_id = request.env['shuttles.model'].search(
                 [('driver_login_id', '=', driver_id)])
-            #search for today schedule
+            #search for today schedule which are not confirmed today
             shuttle_schedule = request.env['shuttle_schedule.model'].search(
-                [('shuttle_id', '=', shuttle_id.id),('weekday_id.name', '=', datetime.now().strftime('%A').lower())])
+                [('shuttle_id', '=', shuttle_id.id),('weekday_id.name', '=', datetime.now().strftime('%A').lower()),('confirmed_date', '!=', today)])
             #search next 3days schedule
-            today = datetime.now()
             date_list = [today + timedelta(days=i) for i in range(3)]
             next_3_days = [date.strftime('%A').lower() for date in date_list]
             print(next_3_days)
@@ -37,6 +37,7 @@ class ShuttleManagement(http.Controller):
                 ('shuttle_id', '=', shuttle_id.id),
                 ('weekday_id.name', '=', yesterday.strftime('%A').lower()),
                 ('confirmed_date', '!=', yesterday),
+                ('confirmed_date', '!=', today),
             ])
 
         return request.render('shuttle_management.my_shuttle_template',{
@@ -95,14 +96,14 @@ class ShuttleManagement(http.Controller):
             to_confirm_date = datetime.now().date()
             confirm_shuttle_schedule = request.env['shuttle_schedule.model'].search([('id', '=', schedule_id)])
             confirm_shuttle_schedule.write({'confirmed_date': to_confirm_date})
-            if date=='td':
-                to_confirm_date = datetime.now().date()
-                confirm_shuttle_schedule=request.env['shuttle_schedule.model'].search([('id', '=', schedule_id)])
-                confirm_shuttle_schedule.write({'confirmed_date': to_confirm_date})
-            elif date == 'yt':
-                to_confirm_date = (datetime.now() - timedelta(days=1)).date()
-                confirm_shuttle_schedule = request.env['shuttle_schedule.model'].search([('id', '=', schedule_id)])
-                confirm_shuttle_schedule.write({'confirmed_date': to_confirm_date})
+            # if date=='td':
+            #     to_confirm_date = datetime.now().date()
+            #     confirm_shuttle_schedule=request.env['shuttle_schedule.model'].search([('id', '=', schedule_id)])
+            #     confirm_shuttle_schedule.write({'confirmed_date': to_confirm_date})
+            # elif date == 'yt':
+            #     to_confirm_date = (datetime.now() - timedelta(days=1)).date()
+            #     confirm_shuttle_schedule = request.env['shuttle_schedule.model'].search([('id', '=', schedule_id)])
+            #     confirm_shuttle_schedule.write({'confirmed_date': to_confirm_date})
 
         return request.redirect('/my_shuttle')
 
