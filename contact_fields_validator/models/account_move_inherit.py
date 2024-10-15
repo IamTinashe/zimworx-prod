0ccfab1a8a8be4372fd0fa91774be3ccf597d42f
IamTinashe <tinashetp@gmail.com>
2024-10-15 18:13:47 +0200

    makes_modifications

--- contact_fields_validator/models/account_move_inherit.py
+++ contact_fields_validator/models/account_move_inherit.py
@@ -1,15 +1,16 @@
 from odoo import models
 from .partner_field_validator import PartnerFieldValidator  # Ensure correct import
 
+
 class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
 
     def write(self, vals):
-        from .partner_field_validator import PartnerFieldValidator  # Local import to avoid circular reference
         res = super(AccountMoveInherit, self).write(vals)
         self._check_missing_partner_fields()
         return res
 
     def _check_missing_partner_fields(self):
-        validator = PartnerFieldValidator(self.partner_id)
-        validator.validate_fields()  # Call the validate method
+        if self.partner_id.x_studio_primary_business_relationship == 'Prospect/Customer':
+            validator = PartnerFieldValidator(self.partner_id)
+            validator.validate_fields()  # Call the validate method
